b6c18b2a68811174a2129727a52778473baff55c
Caio Pereira <caioefg@gmail.com>
2023-07-03 11:49:04 -0300

    Fix: alterando componente para uso do zustand

--- frontend/src/app/home/productCard/page.tsx
+++ frontend/src/app/home/productCard/page.tsx
@@ -1,35 +1,36 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import ProductCard from '../../../app/componentes/prodCard';
-import { number } from 'zod';
 import { useRouter } from 'next/navigation'
+import { useTotalStore } from '../../../store/total'
 
 
 export default function ProductList({ products }: any) {
-  const [ total, setTotal ] = useState<number>(0)
+  // const [ total, setTotal ] = useState<number>(0)
   const router = useRouter()
 
+  const { state: { total, cart }, actions: { setValue  } } = useTotalStore();
+
   useEffect(() => {
-    const cartString = localStorage.getItem('cart') || '';
-    if (cartString) {
-      const cart = JSON.parse(cartString);
-      const cartTotal = cart.reduce((acc: any, item: any) => acc + item.total, 0);
-      setTotal((prevTotal: any) => prevTotal + cartTotal);
+    // const cartData = JSON.parse(localStorage.getItem('cart') || '[]');
+    if (cart.length > 0) {
+      const cartTotal = cart.reduce((acc: number, item: any) => acc + item.total, 0);
+      // setValue(cartTotal);
     }
-  }, [setTotal, ]);
+  }, [total]);
   
-  const handleClick = () => {
+/*   const handleClick = () => {
     router.push('/cart')
   }
-  
+   */
   return (
     <div>
       {products &&
         products?.map((product: any, i: number) => (
-          <ProductCard product={product} key={i} setTotal={setTotal} total={total}/>
+          <ProductCard product={product} key={i} />
         ))}
 
-    <button className='fixed bottom-0 right-0 mb-12 mr-12' onClick={handleClick} value={total}>Total: {total}</button>
+    <button className='fixed bottom-0 right-0 mb-12 mr-12' onClick={() => router.push('/cart')} value={total}>Total: {total}</button>
     </div>
   );
 }
